6d76a365bae3bcb44ea805de890ef43ff60245ba
ignacio <iaraya19@hotmail.com>
2024-11-18 20:21:21 -0300

    Update config.h

--- NAMPluginSimplified/config.h
+++ NAMPluginSimplified/config.h
@@ -25,7 +25,7 @@
 #define PLUG_DOES_STATE_CHUNKS 0
 #define PLUG_HAS_UI 1
 #define PLUG_WIDTH 600
-#define PLUG_HEIGHT 600
+#define PLUG_HEIGHT 300
 #define PLUG_FPS 60
 #define PLUG_SHARED_RESOURCES 0
 #define PLUG_HOST_RESIZE 0
@@ -57,3 +57,7 @@
 #define APP_SIGNAL_VECTOR_SIZE 64
 
 #define ROBOTO_FN "Roboto-Regular.ttf"
+
+#define FIXED_MODEL "model.nam" 
+#define FIXED_IR "ir.wav"
+#define BACKGROUND_FN "background.jpg"
